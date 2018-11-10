388a96547057977b90c39aa1737e190f2ce80caf
Malte Kießling <mkalte666@googlemail.com>
2018-11-10 22:24:35 +0100

    added brain to the copy command for all files. im more or less happy with this now

---
+++ cmake/update_directory.cmake
@@ -0,0 +1,34 @@
+#source: https://github.com/tikonen/blog/blob/master/cmake/update_directory/cmake/update_directory.cmake
+
+#The MIT License (MIT)
+#
+#Copyright (c) 2012-2017 Teemu Ikonen
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+file(GLOB_RECURSE _file_list RELATIVE "${src_dir}" "${src_dir}/*")
+
+foreach( each_file ${_file_list} )
+  set(destinationfile "${dst_dir}/${each_file}")
+  set(sourcefile "${src_dir}/${each_file}")
+  if(NOT EXISTS ${destinationfile} OR ${sourcefile} IS_NEWER_THAN ${destinationfile})
+    get_filename_component(destinationdir ${destinationfile} DIRECTORY)
+    file(COPY ${sourcefile} DESTINATION ${destinationdir})
+  endif()
+endforeach(each_file)
